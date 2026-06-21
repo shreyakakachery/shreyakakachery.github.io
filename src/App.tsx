4df86512d92abb670ecb76b55c8c1f1aa7857d4b
Shreya Kakachery <shreyakakachery@gmail.com>
2026-06-21 09:17:19 -0700

    Create and render ExperienceCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import InfoContainer from './components/InfoContainer/InfoContainer.tsx'
-import Footer from './components/Footer/Footer.tsx'
-import profileData from './data/profile.json'
-import './App.scss'
-import EducationCard from './components/EducationCard/EducationCard.tsx'
+import InfoContainer from "./components/InfoContainer/InfoContainer.tsx";
+import EducationCard from "./components/EducationCard/EducationCard.tsx";
+import ExperienceCard from "./components/ExperienceCard/ExperienceCard.tsx";
+import Footer from "./components/Footer/Footer.tsx";
+import profileData from "./data/profile.json";
+import "./App.scss";
 
 function App() {
-
   return (
     <div className="app">
-      <InfoContainer 
-        title="Education" 
+      <InfoContainer
+        title="Education"
         dataList={profileData.education}
-        CardComponent={EducationCard} 
+        CardComponent={EducationCard}
+      />
+      <InfoContainer
+        title="Experience"
+        dataList={profileData.experience}
+        CardComponent={ExperienceCard}
       />
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ExperienceCard/ExperienceCard.tsx
@@ -0,0 +1,52 @@
+import './ExperienceCard.scss'
+
+interface ExperienceCardProps {
+    role: string;
+    location1: string;
+    location2: string | null;
+    start: string;
+    end: string;
+    description: string;
+    skills: string[];
+}
+
+export default function ExperienceCard({
+    role, 
+    location1, 
+    location2,
+    start, 
+    end, 
+    description, 
+    skills
+}: ExperienceCardProps) {
+    return (
+        <div className="portfolio-card experience-card">
+            <div className="card-header">
+                <h3 className="title">
+                    {role}
+                </h3>
+                <span className="date-badge">
+                    {start} - {end}
+                </span>
+            </div>
+
+            <h4 className="subtitle">
+                {location1} {location2 && <span className="location-divider">| {location2}</span>} 
+            </h4>
+
+            <p className="description">
+                {description}
+            </p>
+
+            {skills && skills.length >0 && (
+                <div className="tags-container">
+                    {skills.map((skill, index) => (
+                        <span key={index} className="tag">
+                            {skill}
+                        </span>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+}
