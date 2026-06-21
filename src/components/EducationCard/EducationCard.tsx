43702202d45553acdce4da7a1c3705a78e5bfcf8
Shreya Kakachery <shreyakakachery@gmail.com>
2026-06-21 13:29:35 -0700

    Add type interface to EducationCard component

--- src/components/EducationCard/EducationCard.tsx
+++ src/components/EducationCard/EducationCard.tsx
@@ -1,19 +1,27 @@
-import './EducationCard.scss';
+import "./EducationCard.scss";
 
-export default function EducationCard({ program, institution, start, end}) {
-    return (
-        <div className="portfolio-card education-card"> 
-            <div className="card-header">
-                <h3 className="title">
-                    {program}
-                </h3>
-                <span className="duration">
-                    {start} - {end}
-                </span>
-                <h4 className="subtitle">
-                    {institution}
-                </h4>
-            </div>
-        </div>
-    );
+interface EducationCardProps {
+  program: string;
+  institution: string;
+  start: string;
+  end: string;
+}
+
+export default function EducationCard({
+  program,
+  institution,
+  start,
+  end,
+}: EducationCardProps) {
+  return (
+    <div className="portfolio-card education-card">
+      <div className="card-header">
+        <h3 className="title">{program}</h3>
+        <span className="duration">
+          {start} - {end}
+        </span>
+        <h4 className="subtitle">{institution}</h4>
+      </div>
+    </div>
+  );
 }
